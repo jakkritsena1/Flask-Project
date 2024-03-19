e9d5edcec96363a60249aca07dc51a48e4d3cedc
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-03-19 19:12:44 +0700

    databases

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template,request
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='flask_project'
+app.config['MYSQL_DATABASE_HOST']='localhost'
 
 @app.route('/', methods=['POST','GET'])
 def login():
