b15e0cf9ebec954fba75240f53500759df618315
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-03-19 19:40:13 +0700

    edit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask,render_template
-# from flaskext.mysql import MySQL
+from flask import Flask, render_template
+from flask_mysql import MySQL
 
-# mysql = MySQL()
+mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='flask_project'
-app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'flask_project'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-@app.route('/', methods=['POST','GET'])
+mysql.init_app(app)
+
+@app.route('/', methods=['POST', 'GET'])
 def login():
-        return render_template('/index.html')
-    
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
