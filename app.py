80b5dbd730a9d15dd5b1765d8814dbd1a7cea3e1
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-03-19 23:34:43 +0700

    19-3-2567

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_project'
+db = SQLAlchemy(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask_project'
+class User(db.Model):
+    name = db.Column(db.String(255), nullable=False)
+    lastname = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False,  primary_key=True)
+    password = db.Column(db.String(255), nullable=False)
 
-mysql = MySQL(app)
+    __tablename__ = 'users'
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/', methods=["POST", "GET"])
 def login():
+    error = ''
     if request.method == "POST":
-        uname = request.form["User"]
-        passw = request.form["Password"]
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE username = %s AND password = %s", (uname, passw))
-        user = cur.fetchone()
-        cur.close()
+        uname = request.form["username"]
+        passw = request.form["password"]
+        user = User.query.filter_by(username=uname, password=passw).first()
         if user:
             return render_template('home.html')
         else:
             error = "Invalid username or password"
-            return render_template('home.html')
+        
+    return render_template('index.html',error=error)
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask_project.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2024 at 05:30 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flask_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`name`, `lastname`, `username`, `password`) VALUES
+('Jakkrit', 'Sena', 'ballsna', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
