db1d9e73e0139958d991a02fcb837f8ba32fb7cc
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-03-23 00:12:36 +0700

    update

--- export_graph.py
+++ export_graph.py
@@ -1,39 +1,41 @@
-import numpy as np
+﻿import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-dataset = pd.read_csv("/dataset/dataset.csv")
-x = dataset[dataset['d_eng_speed']]
-y = dataset[dataset['d_tps_pct']]
-# x = np.linspace(-5, 5, 100)
-# y = np.linspace(-5, 5, 100)
-x, y = np.meshgrid(x, y)
-z = np.sin(np.sqrt(x**2 + y**2))
+class report: 
+    
+    def plot_3d_surface(self,predict):
+        dataset = pd.read_csv("C:\\Users\\Ball.sna\\source\\repos\\jakkritsena1\\Flask-Project\\dataset\\dataset.csv")
+        tpsPCT = 'd_tps_pct'
+        RPM = 'd_eng_speed'
+
+        x = dataset.loc[[RPM]]
+        y = dataset.loc[[tpsPCT]]
+    
+        x, y = np.meshgrid(x, y)
+        z = predict
+
+        data = {'X': x.flatten(), 'Y': y.flatten(), 'Z': z.flatten()}
+        df = pd.DataFrame(data)
+
+        fig = plt.figure(figsize=(12, 6))
+        ax1 = fig.add_subplot(121, projection='3d')
+        ax1.plot_surface(x, y, z, cmap='viridis')
+        ax1.set_xlabel('X')
+        ax1.set_ylabel('Y')
+        ax1.set_zlabel('Z')
+        ax1.set_title('Surface 3D Plot')
+
+        ax2 = fig.add_subplot(122)
+        scatter = ax2.scatter(df['X'], df['Y'], c=df['Z'], cmap='viridis')
+        ax2.set_xlabel('X')
+        ax2.set_ylabel('Y')
+        ax2.set_title('Table with Color Coded Height')
+        fig.colorbar(scatter, ax=ax2, label='Z (Color by Height)')
+
+        plt.tight_layout()
+        plt.show()
 
 
-data = {'X': x.flatten(), 'Y': y.flatten(), 'Z': z.flatten()}
-df = pd.DataFrame(data)
-
-
-fig = plt.figure(figsize=(12, 6))
-ax1 = fig.add_subplot(121, projection='3d')
-
-
-ax1.plot_surface(x, y, z, cmap='viridis')
-ax1.set_xlabel('X')
-ax1.set_ylabel('Y')
-ax1.set_zlabel('Z')
-ax1.set_title('Surface 3D Plot')
-
-
-ax2 = fig.add_subplot(122)
-scatter = ax2.scatter(df['X'], df['Y'], c=df['Z'], cmap='viridis')
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-ax2.set_title('Table with Color Coded Height')
-fig.colorbar(scatter, ax=ax2, label='Z (Color by Height)')
-
-
-plt.tight_layout()
-plt.show()
+r = report()
+r.plot_3d_surface(1500,80) 
---
+++ model.py
@@ -0,0 +1,99 @@
+﻿import pandas as pd
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from tensorflow import keras
+df = pd.read_csv("C:\\Users\\Ball.sna\\source\\repos\\jakkritsena1\\Flask-Project\\dataset\\dataset.csv")
+class Model_Engine:
+    def FrontModel(self, speed, tps):
+        inj = 'd_inj_pw_front'
+        O2use = 'd_front_o2'
+        tpsPCT = 'd_tps_pct'
+        map = 'd_map_v'
+        ve = 'd_ve_front'
+        veNew = 'd_new_ve_front'
+        RPM = 'd_eng_speed'
+
+        condition = ((df[RPM] >= 0) & (df[RPM] <= speed) & (df[tpsPCT] <= tps))
+        X = df.loc[condition, [tpsPCT,map,RPM,inj,O2use]]
+        y = df.loc[condition, [veNew]]
+
+        X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0)
+
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        model = keras.Sequential([
+        keras.layers.Input(shape=(5,)),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01)),
+        keras.layers.Dense(1)
+                                ])
+        model.compile(optimizer='adam', loss=['mean_squared_error'], metrics=['mean_absolute_error'])
+        model.fit(X_train, y_train, epochs=1000,
+              batch_size=64, validation_split=0.3)
+
+        predictions = model.predict(X_test)
+        predictions_avg = np.mean(predictions)
+
+        mse = mean_squared_error(y_test, predictions)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test, predictions)
+
+        print("ค่าเฉลี่ยของค่าที่ทำนายได้:", predictions_avg)
+        print("Mean squared error :", mse)
+        print("Root Mean Squared Error :", rmse)
+        print("Mean Absolute Error (MAE):", mae)
+
+
+       
+     
+
+
+    def RearModel(self, speed, tps):
+        inj = 'd_inj_pw_rear'
+        O2use = 'd_rear_o2'
+        tpsPCT = 'd_tps_pct'
+        map = 'd_map_v'
+        ve = 'd_ve_rear'
+        veNew = 'd_new_ve_rear'
+        RPM = 'd_eng_speed'
+
+        condition = ((df[RPM] >= 0) & (df[RPM] <= speed) & (df[tpsPCT] <= tps))
+        X = df.loc[condition, [tpsPCT,map,RPM,inj,O2use]]
+        y = df.loc[condition, [veNew]]
+
+        X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=0)
+
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        model = keras.Sequential([
+        keras.layers.Input(shape=(5,)),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01)),
+        keras.layers.Dense(1)
+                                ])
+        model.compile(optimizer='adam', loss=['mean_squared_error'], metrics=['mean_absolute_error'])
+        model.fit(X_train, y_train, epochs=1000,
+              batch_size=64, validation_split=0.3)
+
+        predictions = model.predict(X_test)
+        predictions_avg = np.mean(predictions)
+
+        mse = mean_squared_error(y_test, predictions)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test, predictions)
+
+        print("ค่าเฉลี่ยของค่าที่ทำนายได้:", predictions_avg)
+        print("Mean squared error :", mse)
+        print("Root Mean Squared Error :", rmse)
+        print("Mean Absolute Error (MAE):", mae)
