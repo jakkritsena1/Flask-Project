92d38deb2d3727feb38e2b1bb44db1adcdd37371
jakkritsena1 <112282385+jakkritsena1@users.noreply.github.com>
2024-03-24 00:45:13 +0700

    -

--- app.py
+++ app.py
@@ -6,12 +6,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask_project'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    name = db.Column(db.String(255), nullable=False)
-    lastname = db.Column(db.String(255), nullable=False)
-    username = db.Column(db.String(255), unique=True, nullable=False,  primary_key=True)
-    password = db.Column(db.String(255), nullable=False)
+        __tablename__ = 'users'
+        name = db.Column(db.String(255), nullable=False)
+        lastname = db.Column(db.String(255), nullable=False)
+        username = db.Column(db.String(255), unique=True, nullable=False,  primary_key=True)
+        password = db.Column(db.String(255), nullable=False)
+        
+class Front_engine(db.Model):
+        __tablename__ = 'front_engine'
+        rpm = db.Column(db.Double(), unique=True, nullable=False, primary_key=True)
+        o2 = db.Column(db.Double(), nullable=False)
+        o2_v = db.Column(db.Double(), nullable=False)
+        inj = db.Column(db.Double(), nullable=False)
+        adv = db.Column(db.Double(), nullable=False)
+        tps_v = db.Column(db.Double(), nullable=False)
+        map = db.Column(db.Double(), nullable=False)
+        map_v = db.Column(db.Double(), nullable=False)
+        ve = db.Column(db.Double(), nullable=False)
 
-    __tablename__ = 'users'
+class Rear_engine(db.Model):
+        __tablename__ = 'rear_engine'
+        rpm = db.Column(db.Double(), unique=True, nullable=False, primary_key=True)
+        o2 = db.Column(db.Double(), nullable=False)
+        o2_v = db.Column(db.Double(), nullable=False)
+        inj = db.Column(db.Double(), nullable=False)
+        adv = db.Column(db.Double(), nullable=False)
+        tps_v = db.Column(db.Double(), nullable=False)
+        map = db.Column(db.Double(), nullable=False)
+        map_v = db.Column(db.Double(), nullable=False)
+        ve = db.Column(db.Double(), nullable=False)
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -27,6 +50,8 @@ def login():
             error = "Invalid username or password"
         
     return render_template('index.html',error=error)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
